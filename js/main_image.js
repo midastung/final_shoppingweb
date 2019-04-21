e13213c035263a9a5008122035f2501752d8f184
Midas <midas112800@gmail.com>
2019-04-21 21:01:02 +0800

    yoyo

---
+++ js/main_image.js
@@ -0,0 +1,27 @@
+var slids=0;
+	function slid(page){
+		slids+=page;
+		if(slids <0){
+			slids=document.getElementsByClassName("main_images").length-1;
+		}else if(slids >=document.getElementsByClassName("main_images").length){
+			slids=0;
+		}
+		for(var i=0;i<document.getElementsByClassName("main_images").length;i++ ){
+			document.getElementsByClassName("main_images")[i].className="main_images";
+		}
+		document.getElementsByClassName("main_images")[slids].className="main_images show";
+	}
+	function autoslid(){
+		slid(1);
+		setTimeout('autoslid()',5000);
+	}
+	setTimeout('autoslid()',5000);
+function videoss(){
+	if(document.getElementsByClassName("videoss")[0].style.height==""||document.getElementsByClassName("videoss")[0].style.height=="0px"){
+		document.getElementsByClassName("videoss")[0].style.height="500px";
+		
+	}else{
+		document.getElementsByClassName("videoss")[0].style.height="0px";
+	}
+	
+}
